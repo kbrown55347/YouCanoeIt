1e8ba88d3fc1ffb0830a0684ba221d794ac9b010
Kelsey Brown <kbrown55347@gmail.com>
2021-12-29 09:09:52 -0600

    wired and tested past trips router to get users past trips from database

--- checklist.md
+++ checklist.md
@@ -9,6 +9,6 @@ To Do:
     - [x] setup router to DB for past trips
     - [x] setup dispatch on user page to trigger GET past trips route
     - [ ] setup saga function to GET past trip information from db
-    - [ ] setup put route of db information from saga function to reducer
+    - [ ] setup route of db information from saga function to reducer
     - [ ] map through reducer to display past trips
 
--- server/routes/past_trips.router.js
+++ server/routes/past_trips.router.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET past trips route code
+// This route returns logged in users past trips
+router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user', req.user);
-  res.sendStatus(200);
+
+  let queryText;
+  let queryValues;
+
+  // get past trip info from database, but only for column names listed
+  queryText = `
+      SELECT "id", "trip_name", "start_date", "end_date", "image_url", "user_id" FROM "trips"
+        WHERE "user_id"=$1
+    `;
+  queryValues = [req.user.id]
+
+  pool.query(queryText, queryValues)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 /**
