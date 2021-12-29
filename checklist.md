b235160d95e9aca659101af51b747a919ecf458a
Kelsey Brown <kbrown55347@gmail.com>
2021-12-29 08:31:27 -0600

    added MUI grid and button to user page

--- checklist.md
+++ checklist.md
@@ -7,7 +7,7 @@ To Do:
     - [x] add dummy data to dbjh
     - [x] setup past trips reducer
     - [x] setup router to DB for past trips
-    - [ ] setup dispatch on user page to trigger GET past trips route
+    - [x] setup dispatch on user page to trigger GET past trips route
     - [ ] setup saga function to GET past trip information from db
     - [ ] setup put route of db information from saga function to reducer
     - [ ] map through reducer to display past trips
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
+// MUI imports
+import { Button, Grid } from '@mui/material';
 
 function UserPage() {
   const user = useSelector((store) => store.user);
@@ -9,14 +11,32 @@ function UserPage() {
 
   // on page load fetch past trips
   useEffect(() => {
-    dispatch({type: 'FETCH_PAST_TRIPS'})
+    dispatch({ type: 'FETCH_PAST_TRIPS' })
   }, []);
+
+
+  // add function to send user to add trip page on click of button
 
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <h3>Welcome, {user.username}!</h3>
+
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <h2 className="page-title">Past Trips</h2>
+        {/* Add Trip button */}
+        <Button
+          variant="contained"
+          style={{ backgroundColor: '#8fa253', color: 'white' }}
+        // onClick={function to send user to add trip page}
+        > Add Trip
+        </Button>
+      </Grid>
+
     </div>
   );
 }
