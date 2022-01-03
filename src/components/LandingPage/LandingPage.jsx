81bc0a3fa0fd646490ea5cc53ac055dba55a62cd
Kelsey Brown <kbrown55347@gmail.com>
2022-01-03 11:49:17 -0600

    added margin to register and login forms

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,9 +4,6 @@ import './LandingPage.css';
 // MUI imports
 import { Button, Grid } from '@mui/material';
 
-// CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
-
 function LandingPage() {
   const [heading, setHeading] = useState('Welcome');
   const history = useHistory();
@@ -60,7 +57,7 @@ function LandingPage() {
         src="https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/elymn/DSC01124_a6403509-4789-4f3d-9e31-0848dd4b782a.jpg"
         alt="BWCA image">
       </img>
-      <p>The Boundary Waters Canoe Area (BWCA) Wilderness in northeastern Minnesota spans over 1 million acres of natural lakes and land. After successfully completing a camping and canoeing trip in this beautiful area, you can document it using YouCanoeIt. YouCanoeIt allows you to post, view, edit and delete BWCA trips. You can include details such as lakes traveled, longest portage, entry/exit points and more. Use the application to refer back to your past trips when planning for upcoming trips, when wanting to share suggestions and experiences with others, or when just feeling nostalgic!</p>
+      <p>The Boundary Waters Canoe Area (BWCA) Wilderness spans over 1 million acres of natural lakes and land in northeastern Minnesota. After successfully completing a camping and canoeing trip in this beautiful area, you can document it using YouCanoeIt. YouCanoeIt allows you to post, view, edit and delete BWCA trips. You can include details such as lakes traveled, longest portage, entry/exit points and more. Use the application to refer back to your past trips when planning for upcoming trips, when wanting to share suggestions and experiences with others, or when just feeling nostalgic!</p>
 
     </div>
   );
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -20,6 +20,7 @@ function LoginPage() {
           Register
         </button>
       </center>
+
     </div>
   );
 }
