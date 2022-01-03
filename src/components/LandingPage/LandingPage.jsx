e906e6862118d31cbf3dfd6f69c465c79e96c6e5
Kelsey Brown <kbrown55347@gmail.com>
2022-01-03 10:47:38 -0600

    added and formatted login and register buttons at top of page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
+// MUI imports
+import { Button, Grid } from '@mui/material';
 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
@@ -13,26 +15,48 @@ function LandingPage() {
     history.push('/login');
   };
 
+  // on click of Register, send to registration page
+  const onRegister = (event) => {
+    history.push('/registration');
+  };
+
   return (
     <div className="container">
       <h2>{heading}</h2>
 
-      <div className="grid">
-        <div className="grid-col grid-col_8">
+    {/* Login Button */}
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <p>Already a Member?</p>
+        <Button
+          variant="contained"
+          style={{ backgroundColor: '#8fa253', color: 'white' }}
+          onClick={onLogin}
+        >Login</Button>
+      </Grid>
+
+      {/* Register Button */}
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <p>New Member?</p>
+        <Button
+          variant="contained"
+          style={{ backgroundColor: '#8fa253', color: 'white' }}
+          onClick={onRegister}
+        >Register</Button>
+      </Grid>
+
+
           <p>About App</p>
 
-        </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
-      </div>
     </div>
   );
 }
