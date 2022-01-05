840fab750fa276ba6a973bc0e1af732bc89ec6c5
Kelsey Brown <kbrown55347@gmail.com>
2022-01-05 16:04:00 -0600

    added inputs from add trip component to edit trip component

--- checklist.md
+++ checklist.md
@@ -5,13 +5,7 @@
 *trip details page*
 - [x] wire edit button to send user and trip id to edit page
 *edit trip page*
-- [x] setup useEffect to fire dispatch to saga function to
-fetch trip to edit (w/payload of params.id)
-- [ ] setup saga function to get trip to edit from DB
-- [ ] create trip to edit reducer
-- [ ] setup GET route to get trip info from DB
-- [ ] in saga function, send trip info to trip to edit reducer 
-
+- [x] setup useEffect to fire dispatch to trip details saga function to fetch trip details (w/payload of params.id)
 - [ ] setup local state w/ start values equal to trip to edit reducer
 - [ ] bundle local state into object (like add page)
 - [ ] create saga function w/ axios PUT route
--- src/components/EditTrip/EditTrip.jsx
+++ src/components/EditTrip/EditTrip.jsx
@@ -8,37 +8,6 @@ function EditTrip() {
     const dispatch = useDispatch();
     const history = useHistory();
 
-    /* access trip details reducer of trip that was previously
-    clicked on to view details */
-    const tripDetails = useSelector(store => store.tripDetails);
-    console.log('in EditTrip', tripDetails);
-
-    const params = useParams();
-
-    // // local states to collect trip info
-    // // set initial value of state to equal info from tripDetails reducer
-    // let [tripName, setTripName] = useState(tripDetails.trip_name);
-    // let [startDate, setStartDate] = useState(tripDetails.start_date);
-    // let [endDate, setEndDate] = useState(tripDetails.end_date);
-    // let [entryPoint, setEntryPoint] = useState(tripDetails.entry_point);
-    // let [exitPoint, setExitPoint] = useState(tripDetails.exit_point);
-    // let [longestPortage, setLongestPortage] = useState(tripDetails.longest_portage);
-    // let [lakes, setLakes] = useState(tripDetails.lakes);
-    // let [tripComments, setTripComments] = useState(tripDetails.comments);
-    // let [imagePath, setImagePath] = useState(tripDetails.image_url);
-    // let [imageDescription, setImageDescription] = useState(tripDetails.image_description);
-
-    // let tripId = tripDetails.id;
-
-    // // bundle trip edits into object
-    // const tripEdits = {
-    //     tripId, tripName, startDate, endDate,
-    //     entryPoint, exitPoint, longestPortage,
-    //     lakes, tripComments, imagePath,
-    //     imageDescription
-    // };
-    // console.log(tripEdits);
-
     // on page load fetch trip details for trip to edit
     useEffect(() => {
         // get trip details
@@ -48,10 +17,191 @@ function EditTrip() {
         })
     }, []);
 
+    /* access trip details reducer */
+    const tripDetails = useSelector(store => store.tripDetails);
+    console.log('in EditTrip', tripDetails);
+
+    const params = useParams();
+
+    // local states to collect trip info
+    // set initial value of state to equal info from tripDetails reducer
+    let [tripName, setTripName] = useState(tripDetails.trip_name);
+    let [startDate, setStartDate] = useState(tripDetails.start_date);
+    let [endDate, setEndDate] = useState(tripDetails.end_date);
+    let [entryPoint, setEntryPoint] = useState(tripDetails.entry_point);
+    let [exitPoint, setExitPoint] = useState(tripDetails.exit_point);
+    let [longestPortage, setLongestPortage] = useState(tripDetails.longest_portage);
+    let [lakes, setLakes] = useState(tripDetails.lakes);
+    let [tripComments, setTripComments] = useState(tripDetails.comments);
+    let [imagePath, setImagePath] = useState(tripDetails.image_url);
+    let [imageDescription, setImageDescription] = useState(tripDetails.image_description);
+
+    let tripId = tripDetails.id;
+
+    // console.log('tripName', tripName);
+    // console.log('startDate', startDate);
+
+    // // bundle trip edits into object
+    // const tripEdits = {
+    //     tripId, tripName, startDate, endDate,
+    //     entryPoint, exitPoint, longestPortage,
+    //     lakes, tripComments, imagePath,
+    //     imageDescription
+    // };
+    // console.log('tripEdits object', tripEdits);
+
+
     return (
         <div className="container">
 
             <h2 className="page-title">Edit Trip</h2>
+
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+            >
+                {/* info for trip_name */}
+                <TextField
+                    variant="standard"
+                    type='text'
+                    value={tripName}
+                    label='Trip Name'
+                    style={{ width: '90%' }}
+                    onChange={(event) => setTripName(event.target.value)} />
+            </Grid>
+            <br></br>
+
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-evenly"
+                alignItems="center"
+            >
+                {/* info for start_date */}
+                <TextField
+                    variant="standard"
+                    type='text'
+                    value={startDate}
+                    label='Start Date'
+                    placeholder='MM/DD/YYYY'
+                    style={{ width: '43%' }}
+                    onChange={(event) => setStartDate(event.target.value)} />
+                {/* info for end_date */}
+                <TextField
+                    variant="standard"
+                    type='text'
+                    value={endDate}
+                    label='End Date'
+                    placeholder='MM/DD/YYYY'
+                    style={{ width: '43%' }}
+                    onChange={(event) => setEndDate(event.target.value)} />
+            </Grid>
+            <br></br>
+
+
+
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-evenly"
+                alignItems="center"
+            >
+                {/* info for entry_point */}
+                <TextField
+                    variant="standard"
+                    type='text'
+                    value={entryPoint}
+                    label='Entry Point'
+                    style={{ width: '43%' }}
+                    onChange={(event) => setEntryPoint(event.target.value)} />
+                {/* info for exit_point */}
+                <TextField
+                    variant="standard"
+                    type='text'
+                    value={exitPoint}
+                    label='Exit Point'
+                    style={{ width: '43%' }}
+                    onChange={(event) => setExitPoint(event.target.value)} />
+            </Grid>
+            <br></br>
+
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+            >
+                {/* info for longest_portage */}
+                <TextField
+                    variant="standard"
+                    type='text'
+                    value={longestPortage}
+                    label='Longest Portage'
+                    style={{ width: '90%' }}
+                    onChange={(event) => setLongestPortage(event.target.value)} />
+                <br></br>
+                {/* info for lakes */}
+                <TextField
+                    variant="standard"
+                    type='text' multiline rows={2}
+                    value={lakes}
+                    label='Lakes Traveled'
+                    style={{ width: '90%' }}
+                    onChange={(event) => setLakes(event.target.value)} />
+                <br></br>
+                {/* info for comments */}
+                <TextField
+                    variant="standard"
+                    type='text' multiline rows={2}
+                    value={tripComments}
+                    label='Trip Comments'
+                    style={{ width: '90%' }}
+                    onChange={(event) => setTripComments(event.target.value)} />
+                <br></br>
+                {/* info for image_url */}
+                <TextField
+                    variant="standard"
+                    value={imagePath}
+                    label='Image URL'
+                    style={{ width: '90%' }}
+                    onChange={(event) => setImagePath(event.target.value)} />
+                <br></br>
+                {/* info for image_description */}
+                <TextField
+                    variant="standard"
+                    type='text' multiline rows={2}
+                    value={imageDescription}
+                    label='Image Description'
+                    style={{ width: '90%' }}
+                    onChange={(event) => setImageDescription(event.target.value)} />
+                <br></br>
+            </Grid>
+
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-evenly"
+                alignItems="center"
+            >
+                {/* <Button
+                    variant="contained"
+                    style={{ backgroundColor: '#a1b26a', color: 'white' }}
+                    onClick={handleAddTripClick}>
+                    Add Trip
+                </Button> */}
+
+                {/* <Button
+                    variant="contained"
+                    style={{ backgroundColor: 'white', color: 'black' }}
+                    // send user back to user page on click
+                    onClick={() => {
+                        history.push('/user');
+                    }}>
+                    Cancel
+                </Button> */}
+
+            </Grid>
+
 
         </div>
     )
