08a05cfbc2a0c4fcd90e3e7887207c5b38b7df04
Kelsey Brown <kbrown55347@gmail.com>
2022-01-04 18:28:46 -0600

    added css padding to items on user page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -47,27 +47,26 @@ function UserPage() {
 
       {/* map through pastTrips reducer and append each item to DOM */}
       {pastTrips.map(trip => (
-        <div key={trip.id} className="trips_list">
-          {trip.trip_name}
-          <br></br>
-          {trip.start_date} to {trip.end_date}
-          <br></br>
+        <div key={trip.id} className="trip_list_item">
+          <h4>{trip.trip_name}</h4>
+          <p>{trip.start_date} to {trip.end_date}</p>
+          
           <img
             src={trip.image_url}
             alt='trip image'
           >
           </img>
           <br></br>
-          <Button
-            variant="contained"
-            style={{ backgroundColor: '#a1b26a', color: 'white' }}
-            // send user to trip details page on click of View Details button
-            onClick={() => {
-              history.push(`/trip_details/${trip.id}`);
-            }}
-          >View Details</Button>
-          <br></br>
+            <Button
+              variant="contained"
+              style={{ backgroundColor: '#a1b26a', color: 'white' }}
+              // send user to trip details page on click of View Details button
+              onClick={() => {
+                history.push(`/trip_details/${trip.id}`);
+              }}
+            >View Details</Button>
         </div>
+
       ))
       }
 
